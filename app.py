2cb661490a7b2310dddecd4494a179203aea1064
MatthewHL123 <matthewhasianlumbantoruan@gmail.com>
2023-11-10 14:36:15 +0700

    update

--- app.py
+++ app.py
@@ -3,11 +3,21 @@ from pymongo import MongoClient
 import requests
 from datetime import datetime
 from bson import ObjectId
+import os
+from os.path import join, dirname
+from dotenv import load_dotenv
 
-cxn_str = 'mongodb+srv://test:sparta@cluster0.tcafo85.mongodb.net/?retryWrites=true&w=majority'
-client = MongoClient(cxn_str)
+
+dotenv_path = join(dirname(__file__), '.env')
+load_dotenv(dotenv_path)
+
+MONGODB_URI = os.environ.get("MONGODB_URI")
+DB_NAME =  os.environ.get("DB_NAME")
+
+client = MongoClient(MONGODB_URI)
+db = client[DB_NAME]
+
 app = Flask(__name__)
-db = client.dbsparta_plus_week2
 
 # @app.route('/')
 # def main():
